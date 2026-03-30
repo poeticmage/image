073da8fa168cc933cf4d1d31b981db0243f0465f
Priyadarshi Mukhopadhyay <158742737+poeticmage@users.noreply.github.com>
2026-03-30 17:26:17 +0530

    cors for *

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.options("*", cors());
 const upload = multer();
 
 const API1 = "https://lioninthestreets-fundusimagegate.hf.space/check";
